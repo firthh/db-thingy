cb8ecf3a937ba0ef05c49894efde86b2138ef2ca
Hugo <hugo.firth@gmail.com>
2016-01-06 23:06:33 +0000

    poor first attempt at storing the possible keys in a hash so they can get added to the Command only when they exist

--- src/mysql.rs
+++ src/mysql.rs
@@ -2,15 +2,50 @@ use std::process::Command;
 use yaml_rust::Yaml;
 use database::*;
 
+macro_rules! map(
+    { $($key:expr => $value:expr),+ } => {
+        {
+            let mut m = ::std::collections::HashMap::new();
+            $(
+                m.insert($key, $value);
+            )+
+            m
+        }
+     };
+);
+
 pub struct MySql;
+
+fn add_arg<'t>(cmd: &'t mut Command, arg: &str) -> &'t mut Command {
+	cmd.arg(arg);
+	cmd
+}
+
 
 impl Database for MySql {
     fn new(config: &Yaml) -> DatabaseCommand {
+        let possible_args = map!{
+            "host" => "-h{}",
+            "username" => "-u{}",
+            "user" => "-u{}",
+            "password" => "--password={}",
+            "database" => "{}"
+        };
         let mut cmd = Command::new("mysql");
-        cmd.arg(format!("-h{}", config["host"].as_str().unwrap()))
-            .arg(format!("-u{}", config["username"].as_str().unwrap()))
-            .arg(format!("--password={}", config["password"].as_str().unwrap()))
-            .arg(format!("{}", config["database"].as_str().unwrap()));
+        match config.as_hash() {
+        	Some(h) => {
+        		for (k, v) in h.iter() {
+        			println!("{} {}", possible_args.get(k.as_str().unwrap()).unwrap(), v.as_str().unwrap());
+        		}
+        		println!("done")
+        	},
+        	None => println!("nothing")
+        }
+        
+        add_arg(&mut cmd, &format!("-h{}", config["host"].as_str().unwrap()));
+        add_arg(&mut cmd, &format!("-u{}", config["username"].as_str().unwrap()));
+        add_arg(&mut cmd, &format!("--password={}", config["password"].as_str().unwrap()));
+        add_arg(&mut cmd, &format!("{}", config["database"].as_str().unwrap()));
         DatabaseCommand { cmd: cmd }
     }
 }
