8527e1ee61ed50b168b5e8d5ad286f9e07605c9a
Hugo Firth <hugo.firth@gmail.com>
2016-05-22 14:05:50 +0100

    added nix profile

---
+++ rust/default.nix
@@ -0,0 +1,17 @@
+let
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+
+in stdenv.mkDerivation rec {
+  name = "uswitch-puppet";
+  buildInputs = [
+    pkgs.cargo
+    pkgs.rustc
+  ];
+
+  shellHook = ''
+    
+  '';
+
+}
+
